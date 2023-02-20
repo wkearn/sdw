5bc2445e28ba701e3730a5f9a277fa9d9ae44c2e
William Kearney <wkearn@gmail.com>
2023-02-20 16:22:53 +0100

    Move message counting program to jsfmesgtype.rs

---
+++ src/bin/jsfmesgtype.rs
@@ -0,0 +1,23 @@
+use binrw::io::BufReader;
+use sdw::parser::jsf;
+
+fn main() -> Result<(), binrw::Error> {
+    let args: Vec<String> = std::env::args().collect();
+    let path = &args[1];
+
+    let f = std::fs::File::open(path)?;
+    let reader = BufReader::new(f);
+    let jsf = jsf::JSFFile { reader };
+
+    let mut msg_counts = std::collections::HashMap::new();
+
+    jsf.fold(&mut msg_counts, |counts, msg| {
+        let num = counts.entry(jsf::message_type(&msg.unwrap())).or_insert(0);
+        *num += 1;
+        counts
+    });
+
+    println!("{:?}", msg_counts);
+
+    Ok(())
+}
