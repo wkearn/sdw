541c93f4e57a8819d625967a6f7ea1c963491f0c
William Kearney <wkearn@gmail.com>
2023-02-21 18:09:37 +0100

    Autoformat

--- src/cli/list.rs
+++ src/cli/list.rs
@@ -1,5 +1,5 @@
-use crate::records::SonarDataRecord;
 use crate::parser::jsf;
+use crate::records::SonarDataRecord;
 use binrw::io::BufReader;
 use std::io::{stdout, Write};
 
@@ -11,29 +11,28 @@ pub fn list(path: std::path::PathBuf, output: Option<std::path::PathBuf>) -> std
         Some(path) => {
             let mut writer = std::fs::File::create(path)?;
             for msg in jsf {
-		//let mt = jsf::message_type_string(jsf::message_data(&msg.unwrap()));
-		let rec = SonarDataRecord::from(msg.unwrap());
-		match rec {
-		    SonarDataRecord::Ping(_) => writeln!(writer, "Ping").unwrap(),
-		    SonarDataRecord::Position(_) => writeln!(writer, "Position").unwrap(),
-		    SonarDataRecord::Orientation(_) => writeln!(writer, "Orientation").unwrap(),
-		    SonarDataRecord::Course(_) => writeln!(writer, "Course").unwrap(),
-		    SonarDataRecord::Unknown => writeln!(writer, "Unknown").unwrap()			
-		};
+                //let mt = jsf::message_type_string(jsf::message_data(&msg.unwrap()));
+                let rec = SonarDataRecord::from(msg.unwrap());
+                match rec {
+                    SonarDataRecord::Ping(_) => writeln!(writer, "Ping").unwrap(),
+                    SonarDataRecord::Position(_) => writeln!(writer, "Position").unwrap(),
+                    SonarDataRecord::Orientation(_) => writeln!(writer, "Orientation").unwrap(),
+                    SonarDataRecord::Course(_) => writeln!(writer, "Course").unwrap(),
+                    SonarDataRecord::Unknown => writeln!(writer, "Unknown").unwrap(),
+                };
             }
         }
         None => {
             let mut writer = stdout().lock();
             for msg in jsf {
-		let rec = SonarDataRecord::from(msg.unwrap());
-		match rec {
-		    SonarDataRecord::Ping(_) => writeln!(writer, "Ping").unwrap(),
-		    SonarDataRecord::Position(_) => writeln!(writer, "Position").unwrap(),
-		    SonarDataRecord::Orientation(_) => writeln!(writer, "Orientation").unwrap(),
-		    SonarDataRecord::Course(_) => writeln!(writer, "Course").unwrap(),
-		    SonarDataRecord::Unknown => writeln!(writer, "Unknown").unwrap()			
-		};
-
+                let rec = SonarDataRecord::from(msg.unwrap());
+                match rec {
+                    SonarDataRecord::Ping(_) => writeln!(writer, "Ping").unwrap(),
+                    SonarDataRecord::Position(_) => writeln!(writer, "Position").unwrap(),
+                    SonarDataRecord::Orientation(_) => writeln!(writer, "Orientation").unwrap(),
+                    SonarDataRecord::Course(_) => writeln!(writer, "Course").unwrap(),
+                    SonarDataRecord::Unknown => writeln!(writer, "Unknown").unwrap(),
+                };
             }
         }
     };
