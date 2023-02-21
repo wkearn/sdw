cd72de7c7c0bb2f58504bab25a272f6a65a2ee1a
William Kearney <wkearn@gmail.com>
2023-02-21 18:19:45 +0100

    Args and Actions don't need to be public

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -2,13 +2,13 @@
 use clap::Parser;
 
 #[derive(Parser, Debug)]
-pub struct Args {
+struct Args {
     #[command(subcommand)]
     pub cmd: Action,
 }
 
 #[derive(clap::Subcommand, Debug)]
-pub enum Action {
+enum Action {
     Count {
         path: std::path::PathBuf,
         #[arg(short, long)]
