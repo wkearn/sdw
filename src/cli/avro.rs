3b9ffd5acad3e008456483b88ffc0b4c2ec33a2d
William Kearney <wkearn@gmail.com>
2023-02-21 18:25:33 +0100

    Document the avro function

--- src/cli/avro.rs
+++ src/cli/avro.rs
@@ -6,6 +6,13 @@ use apache_avro::{Schema, Writer};
 
 use binrw::io::BufReader;
 
+/// Convert a JSF file to the Avro format
+///
+/// Currently this writes only the Pings to a single Avro file.
+/// This is meant to be used from the command line interface:
+/// ```console
+/// $ sdw avro <input> <output>
+/// ```
 pub fn avro(path: std::path::PathBuf, output: std::path::PathBuf) -> std::io::Result<()> {
     let raw_schema = r#"{"type": "record","namespace": "sdw","name": "ping","fields": [{"name": "source", "type": "string"},{"name": "timestamp", "type": "long"},{"name": "frequency", "type" : "double"},{"name": "sampling_interval", "type" : "double"},{"name": "channel", "type": "enum", "symbols":["Port","Starboard","Other"],"default":"Other"},{"name": "data", "type":"array","items": "int","default":[]}]}"#;
     let ping_schema = Schema::parse_str(raw_schema).unwrap();
