6ba2f60d14e15dc119c2fcf53cd2ea6681dd1b06
William Kearney <wkearn@gmail.com>
2023-02-21 16:12:39 +0100

    Add output files to count and list actions

--- src/main.rs
+++ src/main.rs
@@ -12,24 +12,50 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
     match args.cmd {
-        Action::Count { path } => {
+        Action::Count { path, output } => {
             let f = std::fs::File::open(path)?;
             let reader = BufReader::new(f);
             let jsf = jsf::JSFFile { reader };
             let counts = jsf::count_jsf_messages(jsf);
-            for (key, value) in &counts {
-                println!("{}\t{}", value, key);
-            }
+
+	    match output {
+		Some(path) => {
+		    let mut writer = std::fs::File::create(path)?;
+		    for (key, value) in &counts {
+			writeln!(writer,"{}\t{}", value, key)?;
+		    };
+		},
+		None => {
+		    let mut writer = stdout().lock();
+		    for (key, value) in &counts {
+			writeln!(writer,"{}\t{}", value, key)?;
+		    };
+		}
+	    };
+	    
+            
         }
-        Action::List { path } => {
+        Action::List { path, output } => {
             let f = std::fs::File::open(path)?;
             let reader = BufReader::new(f);
             let jsf = jsf::JSFFile { reader };
-            let mut lock = stdout().lock();
-            for msg in jsf {
-                let mt = jsf::message_type_string(jsf::message_data(&msg.unwrap()));
-                writeln!(lock, "{}", mt).unwrap();
-            }
+	    match output {
+		Some(path) => {
+		    let mut writer = std::fs::File::create(path)?;
+		    for msg in jsf {
+			let mt = jsf::message_type_string(jsf::message_data(&msg.unwrap()));
+			writeln!(writer, "{}", mt).unwrap();
+		    };	
+		},
+		None => {
+		    let mut writer = stdout().lock();
+		    for msg in jsf {
+			let mt = jsf::message_type_string(jsf::message_data(&msg.unwrap()));
+			writeln!(writer, "{}", mt).unwrap();
+		    };
+		}
+	    };
+            
         }
         Action::Avro { path, output } => {
             let raw_schema = r#"{"type": "record","namespace": "sdw","name": "ping","fields": [{"name": "source", "type": "string"},{"name": "timestamp", "type": "long"},{"name": "frequency", "type" : "double"},{"name": "sampling_interval", "type" : "double"},{"name": "channel", "type": "enum", "symbols":["Port","Starboard","Other"],"default":"Other"},{"name": "data", "type":"array","items": "int","default":[]}]}"#;
