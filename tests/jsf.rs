7fcd5a21d8590eb3854d9405d60453e20b2c76bb
William Kearney <wkearn@gmail.com>
2023-03-02 17:35:22 +0100

    Test the JSF file reader

---
+++ tests/jsf.rs
@@ -0,0 +1,13 @@
+use binrw::io::BufReader;
+use sdw::parser::jsf;
+
+#[test]
+fn read_xtf() -> Result<(), Box<dyn std::error::Error>> {
+    let reader = BufReader::new(std::fs::File::open("assets/HE501_Hydro3_025.001.jsf")?);
+
+    let f = jsf::JSFFile::new(reader);
+
+    assert_eq!(905, f.count());
+
+    Ok(())
+}
