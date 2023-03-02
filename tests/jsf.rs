cd546e650e48c6bca1b4f65ab64327a508b86a6c
William Kearney <wkearn@gmail.com>
2023-03-02 17:49:28 +0100

    Change the name of the tests

--- tests/jsf.rs
+++ tests/jsf.rs
@@ -2,7 +2,7 @@ use binrw::io::BufReader;
 use sdw::parser::jsf;
 
 #[test]
-fn read_xtf() -> Result<(), Box<dyn std::error::Error>> {
+fn read_file() -> Result<(), Box<dyn std::error::Error>> {
     let reader = BufReader::new(std::fs::File::open("assets/HE501_Hydro3_025.001.jsf")?);
 
     let f = jsf::File::new(reader);
--- tests/xtf.rs
+++ tests/xtf.rs
@@ -2,7 +2,7 @@ use binrw::io::BufReader;
 use sdw::parser::xtf;
 
 #[test]
-fn read_xtf() -> Result<(), Box<dyn std::error::Error>> {
+fn read_file() -> Result<(), Box<dyn std::error::Error>> {
     let reader = BufReader::new(std::fs::File::open("assets/15CCT03_SSS_150528172600.xtf")?);
 
     let f = xtf::File::new(reader);
