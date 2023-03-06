dbeb2fde13efb3cbbb6b0a84ab82868cd7b68052
William Kearney <wkearn@gmail.com>
2023-03-06 21:49:55 +0100

    Add a BTreeMap-based index to the Locker

--- src/locker.rs
+++ src/locker.rs
@@ -1,12 +1,19 @@
 //! Lockers for sonar data
-use std::fs::{read_dir, ReadDir};
-use std::io;
+use crate::model::{Channel,SonarDataRecord};
+use crate::parser::jsf;
+use binrw::io::BufReader;
+use std::fs::{read_dir, File};
 use std::path::{Path, PathBuf};
+use time::OffsetDateTime;
+use std::collections::{btree_map,BTreeMap};
+
+type LockerKey = (String,Channel,OffsetDateTime);
+type LockerValue = (PathBuf,usize);
 
 /// A representation of an on-disk sonar data set
 pub struct Locker {
     path: PathBuf,
-    dir: ReadDir,
+    tree: BTreeMap<LockerKey,LockerValue>
 }
 
 impl Locker {
@@ -15,19 +22,33 @@ impl Locker {
     /// ```
     /// # use sdw::locker::Locker;
     /// # fn main() -> Result<(),Box<dyn std::error::Error>> {
-    /// let locker = Locker::open("/home/wkearn/Documents/data/PANGAEA/HE501")?;
+    /// let locker = Locker::open("assets/HE501")?;
     /// # Ok(()) }
     /// ```
     /// # Errors
     ///
     /// This function returns an error when `read_dir` errors
-    pub fn open<P>(path: P) -> io::Result<Self>
+    pub fn open<P>(path: P) -> binrw::BinResult<Self>
     where
         PathBuf: From<P>,
     {
+	let mut tree = BTreeMap::new();
         let path = PathBuf::from(path);
         let dir = read_dir(&path)?;
-        Ok(Locker { path, dir })
+        for entry in dir {
+	    let filepath = entry?.path();
+            let reader = BufReader::new(File::open(&filepath)?);
+            let jsf = jsf::File::new(reader);
+            for (i,msg) in jsf.enumerate() {
+		let key = create_key(SonarDataRecord::from(msg?));
+		let value = (filepath.clone(),i);
+		match key {
+		    Some(k) => tree.insert(k,value),
+		    None => None
+		};		
+            }
+        }
+        Ok(Locker { path, tree})
     }
 
     /// Return a reference to the path of the locker
@@ -36,26 +57,53 @@ impl Locker {
     /// # use sdw::locker::Locker;
     /// # use std::path::Path;
     /// # fn main() -> Result<(),Box<dyn std::error::Error>> {
-    /// let locker = Locker::open("/home/wkearn/Documents/data/PANGAEA/HE501")?;
-    /// assert_eq!(locker.path(),Path::new("/home/wkearn/Documents/data/PANGAEA/HE501"));
+    /// let locker = Locker::open("assets/HE501")?;
+    /// assert_eq!(locker.path(),Path::new("assets/HE501"));
     /// # Ok(()) }
     /// ```
     pub fn path(&self) -> &Path {
         &self.path
     }
 
-    /// Return the directory iterator
-    ///
-    /// ```
-    /// # use sdw::locker::Locker;
-    /// # use std::path::Path;
-    /// # fn main() -> Result<(),Box<dyn std::error::Error>> {
-    /// let locker = Locker::open("/home/wkearn/Documents/data/PANGAEA/HE501")?;
-    /// assert_eq!(122,locker.dir().count());
-    /// # Ok(()) }
-    /// ```
-    pub fn dir(self) -> ReadDir {
-        self.dir
+    /// Get an iterator over the entries of the B-tree, sorted by key
+    pub fn iter(&self) -> Iter {
+	let iter = self.tree.iter();
+	Iter {iter}
+    }
+}
+
+fn create_key<T>(rec: SonarDataRecord<T>) -> Option<(String,Channel,OffsetDateTime)> {
+    match rec {
+        SonarDataRecord::Ping(data) => Some(("Ping".to_string(),data.channel,data.timestamp)),
+        SonarDataRecord::Course(data) => Some(("Course".to_string(),Channel::Other,data.timestamp)),
+        SonarDataRecord::Position(data) => Some(("Position".to_string(),Channel::Other,data.timestamp)),
+        SonarDataRecord::Orientation(data) => Some(("Orientation".to_string(),Channel::Other,data.timestamp)),
+        SonarDataRecord::Unknown => None,
+    }
+}
+
+/// An iterator over the entries of the BTree
+///
+/// This is created by calling `iter` on a `Locker`.
+///
+/// ```
+/// # use sdw::locker::Locker;
+/// # use std::path::Path;
+/// # fn main() -> Result<(),Box<dyn std::error::Error>> {
+/// let locker = Locker::open("assets/HE501")?;
+/// let c = locker.iter().count();
+/// assert_eq!(436719,c);
+/// # Ok(()) }
+/// ```
+pub struct Iter<'a> {
+    iter: btree_map::Iter<'a,LockerKey,LockerValue>
+}
+
+impl<'a> Iterator for Iter<'a> {
+    type Item = (&'a LockerKey, &'a LockerValue);
+	
+    fn next(&mut self) -> Option<Self::Item> {
+	self.iter.next()
     }
 }
 
