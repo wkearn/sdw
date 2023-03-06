61c1dbaa0013db31ed2c915cb51599c084fd1bf1
William Kearney <wkearn@gmail.com>
2023-03-06 15:49:45 +0100

    Add the Locker

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![warn(missing_docs)]
 //! A toolkit for working with imaging sonar data
 pub mod cli;
+pub mod locker;
 pub mod model;
 pub mod parser;
---
+++ src/locker.rs
@@ -0,0 +1,33 @@
+//! Lockers for sonar data
+use std::path::{Path,PathBuf};
+
+/// A representation of an on-disk sonar data set
+pub struct Locker {
+    path: PathBuf
+}
+
+impl Locker {
+    /// Create a new locker
+    pub fn open<P>(path: P) -> Self
+    where
+        PathBuf: From<P>
+    {
+        Locker {path: PathBuf::from(path)}
+    }
+
+    /// Return a reference to the path of the locker
+    pub fn path(&self) -> &Path {
+	&self.path
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn new_locker() {
+	let locker = Locker::open("/home/wkearn/Documents/data/PANGAEA/HE501");
+	assert_eq!(locker.path(),Path::new("/home/wkearn/Documents/data/PANGAEA/HE501"))
+    }
+}
