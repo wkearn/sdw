53886a50c174e9b92d109d151732f09e051b0403
William Kearney <wkearn@gmail.com>
2023-03-06 15:53:15 +0100

    Clarify documentation

--- src/locker.rs
+++ src/locker.rs
@@ -1,23 +1,25 @@
 //! Lockers for sonar data
-use std::path::{Path,PathBuf};
+use std::path::{Path, PathBuf};
 
 /// A representation of an on-disk sonar data set
 pub struct Locker {
-    path: PathBuf
+    path: PathBuf,
 }
 
 impl Locker {
-    /// Create a new locker
+    /// Open a locker at the given path
     pub fn open<P>(path: P) -> Self
     where
-        PathBuf: From<P>
+        PathBuf: From<P>,
     {
-        Locker {path: PathBuf::from(path)}
+        Locker {
+            path: PathBuf::from(path),
+        }
     }
 
     /// Return a reference to the path of the locker
     pub fn path(&self) -> &Path {
-	&self.path
+        &self.path
     }
 }
 
@@ -27,7 +29,10 @@ mod test {
 
     #[test]
     fn new_locker() {
-	let locker = Locker::open("/home/wkearn/Documents/data/PANGAEA/HE501");
-	assert_eq!(locker.path(),Path::new("/home/wkearn/Documents/data/PANGAEA/HE501"))
+        let locker = Locker::open("/home/wkearn/Documents/data/PANGAEA/HE501");
+        assert_eq!(
+            locker.path(),
+            Path::new("/home/wkearn/Documents/data/PANGAEA/HE501")
+        )
     }
 }
