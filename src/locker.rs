f80e58ff4eda5f6a0d9b19a8d4d969faefae7fd0
William Kearney <wkearn@gmail.com>
2023-03-06 18:02:52 +0100

    Add errors section to documentation

--- src/locker.rs
+++ src/locker.rs
@@ -1,12 +1,12 @@
 //! Lockers for sonar data
+use std::fs::{read_dir, ReadDir};
 use std::io;
 use std::path::{Path, PathBuf};
-use std::fs::{read_dir,ReadDir};
 
 /// A representation of an on-disk sonar data set
 pub struct Locker {
     path: PathBuf,
-    dir: ReadDir
+    dir: ReadDir,
 }
 
 impl Locker {
@@ -18,16 +18,16 @@ impl Locker {
     /// let locker = Locker::open("/home/wkearn/Documents/data/PANGAEA/HE501")?;
     /// # Ok(()) }
     /// ```
+    /// # Errors
+    ///
+    /// This function returns an error when `read_dir` errors
     pub fn open<P>(path: P) -> io::Result<Self>
     where
         PathBuf: From<P>,
     {
-	let path = PathBuf::from(path);
-	let dir = read_dir(&path)?;	
-        Ok(Locker {
-	    path,
-	    dir
-        })
+        let path = PathBuf::from(path);
+        let dir = read_dir(&path)?;
+        Ok(Locker { path, dir })
     }
 
     /// Return a reference to the path of the locker
@@ -55,7 +55,7 @@ impl Locker {
     /// # Ok(()) }
     /// ```
     pub fn dir(self) -> ReadDir {
-	self.dir
+        self.dir
     }
 }
 
