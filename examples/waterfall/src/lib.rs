0ec049710484af3663bfa79f77dcd95b1d702499
William Kearney <wkearn@gmail.com>
2023-03-16 21:55:31 +0100

    Get rid of vertex buffer stuff

--- examples/waterfall/src/lib.rs
+++ examples/waterfall/src/lib.rs
@@ -10,57 +10,6 @@ pub mod compute;
 pub mod context;
 pub mod texture;
 
-#[repr(C)]
-#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
-struct Vertex {
-    position: [f32; 3],
-    tex_coords: [f32; 2],
-}
-
-impl Vertex {
-    fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
-        wgpu::VertexBufferLayout {
-            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
-            step_mode: wgpu::VertexStepMode::Vertex,
-            attributes: &[
-                wgpu::VertexAttribute {
-                    // position
-                    offset: 0,
-                    shader_location: 0,
-                    format: wgpu::VertexFormat::Float32x3,
-                },
-                wgpu::VertexAttribute {
-                    // tex_coords
-                    offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
-                    shader_location: 1,
-                    format: wgpu::VertexFormat::Float32x2,
-                },
-            ],
-        }
-    }
-}
-
-const VERTICES: &[Vertex] = &[
-    Vertex {
-        position: [0.0, -1.0, 0.0],
-        tex_coords: [0.0, 0.0],
-    }, // 0
-    Vertex {
-        position: [0.0, 1.0, 0.0],
-        tex_coords: [0.0, 1.0],
-    }, // 1
-    Vertex {
-        position: [1.0, 1.0, 0.0],
-        tex_coords: [1.0, 1.0],
-    }, // 2
-    Vertex {
-        position: [1.0, -1.0, 0.0],
-        tex_coords: [1.0, 0.0],
-    }, // 3
-];
-
-const INDICES: &[u16] = &[2, 1, 0, 2, 0, 3];
-
 struct SonarDataBuffer {
     data: Vec<f32>,
     buffer: wgpu::Buffer,
