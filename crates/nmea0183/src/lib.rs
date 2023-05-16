a6f0c36068a68ef005c8f2cf13cdfca1242a3187
William Kearney <wkearn@gmail.com>
2023-05-16 10:42:56 +0200

    Add a test to count the unique sentence types in the test file
    This adds a few placeholder variants to the Sentence enum so that the
    tests compile.

--- crates/nmea0183/src/lib.rs
+++ crates/nmea0183/src/lib.rs
@@ -2,18 +2,24 @@ use std::str::FromStr;
 
 #[non_exhaustive]
 #[derive(Debug)]
-pub enum Error {
-}
+pub enum Error {}
 
 #[non_exhaustive]
 #[derive(Debug)]
 pub enum Sentence {
+    GLL,
+    VTG,
+    RMC,
+    GGA,
+    ZDA,
 }
 
 impl FromStr for Sentence {
     type Err = Error;
 
-    fn from_str(s: &str) -> Result<Self, <Self as FromStr>::Err> { todo!() }
+    fn from_str(s: &str) -> Result<Self, <Self as FromStr>::Err> {
+        todo!()
+    }
 }
 
 #[cfg(test)]
--- crates/nmea0183/tests/test.rs
+++ crates/nmea0183/tests/test.rs
@@ -1,7 +1,7 @@
 use nmea0183::Sentence;
 use std::path::PathBuf;
 
-fn load_test_data() -> Result<String,std::io::Error> {
+fn load_test_data() -> Result<String, std::io::Error> {
     let mut p = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     p.push("tests/test.txt");
     std::fs::read_to_string(p)
@@ -12,6 +12,50 @@ fn parse_lines() {
     let data = load_test_data().expect("Failed to open test data file");
 
     for line in data.lines() {
-	let nms: Sentence = line.parse().unwrap();
+        let nms: Sentence = line.parse().unwrap();
     }
+}
+
+#[test]
+fn count_types() {
+    let data = load_test_data().expect("Failed to open test data file");
+
+    let mut dict = std::collections::HashMap::new();
+
+    for line in data.lines() {
+        match line.parse().unwrap() {
+            Sentence::GLL => {
+                dict.entry("GLL")
+                    .and_modify(|count| *count += 1)
+                    .or_insert(1);
+            }
+            Sentence::VTG => {
+                dict.entry("VTG")
+                    .and_modify(|count| *count += 1)
+                    .or_insert(1);
+            }
+            Sentence::RMC => {
+                dict.entry("RMC")
+                    .and_modify(|count| *count += 1)
+                    .or_insert(1);
+            }
+            Sentence::GGA => {
+                dict.entry("RMC")
+                    .and_modify(|count| *count += 1)
+                    .or_insert(1);
+            }
+            Sentence::ZDA => {
+                dict.entry("RMC")
+                    .and_modify(|count| *count += 1)
+                    .or_insert(1);
+            }
+	    _ => {}
+        }
+    }
+
+    assert_eq!(Some(&2077), dict.get("GLL"));
+    assert_eq!(Some(&2078), dict.get("VTG"));
+    assert_eq!(Some(&2077), dict.get("RMC"));
+    assert_eq!(Some(&2078), dict.get("GGA"));
+    assert_eq!(Some(&2078), dict.get("ZDA"));
 }
